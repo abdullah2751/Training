2511e58f10f42d1da9c816767d9f22335da8880e
Gl1n- <funnydemscreation@gmail.com>
2019-06-08 17:35:40 +0300

    Added functionality
    Added a counter and a button

--- Training/ViewController.swift
+++ Training/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	var clicks = 0
+	@IBOutlet weak var counterLabel: UILabel!
+	@IBAction func button(_ sender: Any) {
+		clicks += 1
+		counterLabel.text = String(clicks)
+	}
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+		counterLabel.text = String(clicks)
+		
+		
     }
 
 
